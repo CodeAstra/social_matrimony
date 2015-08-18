8c86d324f3f9b72257fe4e07810b391cea944093
vineetsouri <vineetsouri72@gmail.com>
2015-08-18 17:46:44 +0530

    added koala gem and simple form along with adjustments in courosel in land page

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,17 +22,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
-  devise :rememberable, :trackable, 
+  devise :rememberable, :trackable, :registerable,
                             :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
       user.auth_token = auth.credentials.token
       user.auth_expires_at = Time.at(auth.credentials.expires_at)
       # user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
     end
   end
 
@@ -42,5 +42,17 @@ class User < ActiveRecord::Base
         user.email = data["email"] if user.email.blank?
       end
     end
+  end
+
+  def graph
+    @graph ||= Koala::Facebook::API.new(self.auth_token)
+  end
+
+  def friends
+    graph.get_connections("me", "friends")  
+  end
+
+  def profile
+    graph.get_object("me")
   end
 end
