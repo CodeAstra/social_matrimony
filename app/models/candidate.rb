36d3a52d4e7cd2e1f09a9286b149a59db1a49c6f
sriharsha67 <sriharsha67@gmail.com>
2015-08-20 15:14:34 +0530

    Simple form for personal info

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -19,6 +19,8 @@
 #
 
 class Candidate < ActiveRecord::Base
+  attr_accessor :marital_status,:caste,:subcaste,:gothram,:height,:weight,:bodytype,:complexion,:physicalstatus,:salary,:food,:smoke,:drink,:dosham,:star,:raasi,:familytype,:familyvalues,:familystatus
+
   belongs_to :user
 
   def populate!
@@ -69,7 +71,7 @@ def work_from_dump_data
   prof = fb_data["work"]
   prof = prof.collect do |wrk|
           wrk["employer"]["name"]
-         end
+         end if prof
   return Marshal.dump(prof)
 
 end
